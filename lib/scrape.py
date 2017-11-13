ea58234e9ce1ef744eca9e21970cc2d8972204e2
Matthew Deakos <mwoolnerdeakos@gmail.com>
2017-11-12 22:55:04 -0500

    Now can download tweets directly into mongo

--- lib/scrape.py
+++ lib/scrape.py
@@ -2,97 +2,55 @@
 """
 A module to scrape tweets from twitter via the search
 @author Matthew Deakos
-@version 0.2
+@version 0.3
 """
-import csv
-import jsonpickle
 import os
+from os import environ
+import pymongo
 import tweepy
 from tweepy import OAuthHandler
+from datetime import datetime
+from pprint import pprint
 
-def read_certs():
-    """Reads twitter certification from file"""
-    # Get the absolute path of the certs file
-    cert_path = os.path.join(SCRAPE_DIR, 'certs.txt')
-    certs = dict()
-    # Extract API key information
-    with open(cert_path,'r') as cert_file:
-            for line in cert_file:
-                (key, val) = line.split(': ')
-                certs[key] = val.replace('\n','').strip()
-    return certs
+def modify_json(json, query):
+    """Adds a query entry to json"""
+    json['query'] = query
+    json['_id'] = json['id']
+    json.pop('id')
+    return json
 
-def download_tweets(api, query, max_tweets=100000, f='tweets.txt'):
-    """Searches tweets and dumps the json into a text file"""
-    fname = os.path.join(DATA_DIR, f)
-    since_id = None
+
+def download_tweets(query, collection, max_tweets=100000, tweets_per_query=100):
+    """Downloads tweets via the search api and imports it into a mongo collection"""
+    # Get authorization
+    auth = OAuthHandler(environ['CONSUMER_KEY'], environ['CONSUMER_SECRET'])
+    auth.set_access_token(environ['ACCESS_TOKEN'], environ['ACCESS_SECRET'])
+    api = tweepy.API(auth)
 
     max_id = -1
     tweet_count = 0
     print ("Download max {0} tweets".format(max_tweets))
-    with open(fname, 'w') as f:
-        while tweet_count < max_tweets:
-            try:
-                if (max_id <= 0):
-                    if (not since_id):
-                        new_tweets = api.search(q=query, count=100)
-                    else:
-                        new_tweets = api.search(q=query, count=100, since_id=since_id)
-                else:
-                    if (not since_id):
-                        new_tweets = api.search(q=query, count=100, max_id = str(max_id - 1))
+    while tweet_count < max_tweets:
+        try:
+            if (max_id <= 0):
+                new_tweets = api.search(q=query, count=tweets_per_query)
 
-                if not new_tweets:
-                    print("No more tweets found")
-                    break
-                for tweet in new_tweets:
-                    f.write(jsonpickle.encode(tweet._json, unpicklable=False) +
-                        '\n')
+            else:
+                new_tweets = api.search(q=query, count=tweets_per_query,
+                max_id = str(max_id - 1))
 
-                tweet_count += len(new_tweets)
-                print("Downloaded {} tweets".format(tweet_count))
-            except tweepy.TweepError as e:
-                print("Error: " + str(e))
+            if not new_tweets:
+                print("No more tweets found")
                 break
 
+            try:
+                result = collection.insert_many([modify_json(tweet._json, query) for tweet in new_tweets], ordered=False)
+            except Exception as e:
+                print (e)
 
-def tweets_to_csv(infile, outfile):
-    """Converts a json-encoded text file to csv"""
-    # Fix paths to data files
-    in_path = os.path.join(DATA_DIR, infile)
-    out_path = os.path.join(DATA_DIR, outfile)
-
-    f = open(in_path, 'r')
-    tweet_data = []
-    for line in f:
-        tweet_data.append(jsonpickle.decode(line))
-
-    f.close()
-
-    csv_file = open(out_path, 'w', encoding='utf-8', newline='')
-    writer = csv.writer(csv_file)
-    for tweet in tweet_data:
-        line = [tweet['id'],tweet['user']['screen_name'], tweet['user']['followers_count'], tweet['user']['favourites_count'],
-        tweet['user']['statuses_count'],tweet['text'],tweet['favorite_count'],tweet['retweet_count']]
-
-        writer.writerow(line)
-    csv_file.close()
-
-
-def main():
-    certs = read_certs()
-
-    auth = OAuthHandler(certs['consumer_key'], certs['consumer_secret'])
-    auth.set_access_token(certs['access_token'], certs['access_secret'])
-
-    api = tweepy.API(auth)
-
-    download_tweets(api, query='iWatch', max_tweets=500, fname='sweep1.txt')
-
-    print ("Generating CSV...")
-    tweets_to_csv('sweep1.txt', 'sweep1.csv')
-    print ("Done")
-
-
-if __name__ == '__main__':
-    print(os.listdir())
+            tweet_count += len(new_tweets)
+            max_id = new_tweets[-1].id
+            print("Downloaded {} tweets".format(tweet_count))
+        except tweepy.TweepError as e:
+            print("Error: " + str(e))
+            break
